b847149bbfb742fb33324bde259cb765299fca1d
Martin Tříska <triska@unipi.technology>
2019-08-26 18:41:21 +0200

    Update mirror_to_GitHub.sh

--- .gitlab-ci-scripts/mirror_to_GitHub.sh
+++ .gitlab-ci-scripts/mirror_to_GitHub.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-pwd
-
-echo "Script will be there...maybe..."
-
-echo "Printuju commit message ${CI_COMMIT_MESSAGE}"
-
-echo "Printuju vse"
-
-env
 
 #Filter
 git filter-branch -f --index-filter 'git rm -r --cached --ignore-unmatch .project .gitlab-ci.yml .pydevproject .gitlab-ci-scripts' HEAD
 
 #Push
+[[ -d ~/.ssh ]] || mkdir ~/.ssh
+
+echo "$GITHUB_SSH_KEY" | tr -d '\r' > ~/.ssh/github_ssh_key
+chmod 600 ~/.ssh/github_ssh_key
+eval `ssh-agent -s`
+ssh-add ~/.ssh/github_ssh_key
+
+ssh-keyscan -H "github.com" >> ~/.ssh/known_hosts
 git push --mirror git@github.com:martyy665/ekvok.git
+
