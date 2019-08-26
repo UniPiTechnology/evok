1fb9e7729919af62721d74e01081d0eeb323abdf
root <root@martyy>
2019-08-26 17:34:07 +0200

    TEST COMMIT...works in progress

--- .gitlab-ci-scripts/mirror_to_GitHub.sh
+++ .gitlab-ci-scripts/mirror_to_GitHub.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+pwd
+
 echo "Script will be there...maybe..."
+
+echo "Printuju commit message ${CI_COMMIT_MESSAGE}"
+
+echo "Printuju vse"
+
+env
+
+#Filter
+git filter-branch -f --index-filter 'git rm -r --cached --ignore-unmatch .project .gitlab-ci.yml .pydevproject .gitlab-ci-scripts' HEAD
+
+#Push
+git push --mirror git@github.com:martyy665/ekvok.git
