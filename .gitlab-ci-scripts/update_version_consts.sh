7ec46495663696a4e98544dbc2b3570cb220414d
martyy <triska@unipi.technology>
2019-09-09 12:55:49 +0000

    Syntactic changes to make the shellcheck happy...

--- .gitlab-ci-scripts/update_version_consts.sh
+++ .gitlab-ci-scripts/update_version_consts.sh
@@ -4,7 +4,7 @@
 sed -i "s/version=.*/version='${1}',/" setup.py
 
 #Documentation markdown file used by web-interface of gitlab/github
-sed -i "s/\/evok\/archive.*/\/evok\/archive\/${1}.zip/" README.md
+sed -i "s/\\/evok\\/archive.*/\\/evok\\/archive\\/${1}.zip/" README.md
 sed -i "s/unzip.*/unzip ${1}.zip/" README.md
 sed -i "s/cd evok-.*/cd evok-${1}/" README.md
 
