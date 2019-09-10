fec2c60b3ba9b489d2a158f75a04e35b163a9ee1
martyy <triska@unipi.technology>
2019-09-10 09:22:19 +0200

    Add update_version_consts.sh calling from CI

--- .gitlab-ci-scripts/update_version_consts.sh
+++ .gitlab-ci-scripts/update_version_consts.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+if [[ ! -z "${CI_COMMIT_TAG}" ]]; then
+  EVOK_VERSION=${CI_COMMIT_TAG}
+else
+  EVOK_VERSION=`/ci-scripts/generate-new-tag-for-test.sh`
+fi
+
 #Pip package metainfo file
-sed -i "s/version=.*/version='${1}',/" setup.py
+sed -i "s/version=.*/version=\\'${EVOK_VERSION}\\',/" setup.py
 
 #Documentation markdown file used by web-interface of gitlab/github
-sed -i "s/\\/evok\\/archive.*/\\/evok\\/archive\\/${1}.zip/" README.md
-sed -i "s/unzip.*/unzip ${1}.zip/" README.md
-sed -i "s/cd evok-.*/cd evok-${1}/" README.md
+sed -i "s/\\/evok\\/archive.*/\\/evok\\/archive\\/${EVOK_VERSION}.zip/" README.md
+sed -i "s/unzip.*/unzip ${EVOK_VERSION}.zip/" README.md
+sed -i "s/cd evok-.*/cd evok-${EVOK_VERSION}/" README.md
 
 #version.txt file is edited by .git hooks
