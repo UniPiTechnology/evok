a1d519a7f32560ba2aeb2a551111a28be9cba169
martyy <triska@unipi.technology>
2022-08-03 07:28:26 +0000

    Fixed pre-script nesting

--- gitlab-ci/build-before-script.sh
+++ gitlab-ci/build-before-script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "Installing additional packages"
+echo "Installing additional packages by command ${0}"
 apt update && apt install -y dh-virtualenv dpkg-dev dh-exec build-essential fakeroot git python2.7 python libpython2-dev libow-dev
 
-. $(basename "$0")/replace-version-consts.sh
+#. $(basename "$0")/replace-version-consts.sh
+. $(pwd)/replace-version-consts.sh
+
