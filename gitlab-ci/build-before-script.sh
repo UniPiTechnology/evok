ccf2ccd8afc462dafa6634dd0d2706927eef0c77
martyy <triska@unipi.technology>
2022-08-03 14:00:34 +0000

    Remove unwanted bob-default virtualenv binary

--- gitlab-ci/build-before-script.sh
+++ gitlab-ci/build-before-script.sh
@@ -2,6 +2,13 @@
 
 echo "Installing additional packages by command ${0}"
 
+UNWANTED_VIRTUALENV="/usr/local/bin/virtualenv"
+
+if [ -x "${UNWANTED_VIRTUALENV}" ]; then
+  echo "Removing unwanted virtualenv..."
+  rm -f "${UNWANTED_VIRTUALENV}"
+fi
+
 if [ "$DEBIAN_VERSION" == "stretch" ]; then
   apt update && apt install -y dh-virtualenv dpkg-dev dh-exec build-essential fakeroot git python libpython-dev libow-dev
 else
@@ -9,5 +16,5 @@ else
 fi
 
 
-. $(pwd)/gitlab-ci/replace-version-consts.sh
+. "$(pwd)/gitlab-ci/replace-version-consts.sh"
 
