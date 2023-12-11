929d7ff39caa377bbfe6117b9f90d3f79ed523a5
kratochvil <kratochvil@unipi.technology>
2023-12-11 14:26:16 +0100

    WIP: balikovani

--- evok/schemas.py
+++ evok/schemas.py
@@ -2,6 +2,7 @@
 Created on 16 Oct 2017
 
 '''
+from typing import Dict, Tuple
 
 owire_get_out_schema = {
     "$schema": "http://json-schema.org/draft-04/schema#",
@@ -2312,3 +2313,24 @@ owbus_post_out_schema = {
 }
 
 owbus_post_out_example = {"result": {"bus": "/dev/i2c-0", "interval": 300.0, "dev": "owbus", "scan_interval": 0.0, "reset_bus": False, "circuit": "1", "do_scan": False}}
+
+
+schemas: Dict[str, Tuple[dict, dict]] = {
+    'input': (di_post_inp_schema, di_post_inp_example),
+    'output': (relay_post_inp_schema, relay_post_inp_example),
+    'register': (register_post_inp_schema, register_post_inp_example),
+    'ai': (ai_post_inp_schema, ai_post_inp_example),
+    'ao': (ao_post_inp_schema, ao_post_inp_example),
+    'led': (led_post_inp_schema, led_post_inp_example),
+    'watchdog': (wd_post_inp_schema, wd_post_inp_example),
+    '1wdevice': (owire_post_inp_schema, owire_post_inp_example),
+    'owbus': (owbus_post_inp_schema, owbus_post_inp_example),
+}
+schemas['di'] = schemas['input']
+schemas['do'] = schemas['output']
+schemas['relay'] = schemas['output']
+schemas['analoginput'] = schemas['ai']
+schemas['analogoutput'] = schemas['ao']
+schemas['wd'] = schemas['watchdog']
+schemas['temp'] = schemas['1wdevice']
+schemas['sensor'] = schemas['1wdevice']
